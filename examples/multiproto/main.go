472fc3699423070c42fa4f7604b1074ae401a85a
Olehan <alihan.arshabekov@gmail.com>
2018-11-09 18:20:53 +0600

    [feat] regrads to gorilla/mux in README.md

--- examples/multiproto/main.go
+++ examples/multiproto/main.go
@@ -2,15 +2,7 @@ package main
 
 import "github.com/steplems/winter/core"
 
-type WsRouter struct {
-	*core.WebSocketRouter
-}
-
-func (w *WsRouter) Open(ctx *core.WebSocketContext) {
-}
-
 func main() {
-	server := core.NewServer(":5000")
-	server.SetWebSocket("/ws", &WsRouter{})
+	server := core.NewServer(":5549")
 	server.Start()
 }
